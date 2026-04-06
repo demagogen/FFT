7c8f7162ad9f68bd9e2c21fbf760707719e87cc4
demagogen <roman.korennoy@gmail.com>
2026-04-06 16:14:57 +0300

    Remove extra blocks

--- Emulation/one_depth_buffer.py
+++
@@ -1,7 +0,0 @@
-class OneDepthBuffer:
-    def __init__(self):
-
-        # Wire and buses
-        self.scale              = 0
-        self.axis               = 0
-        self.m_axis_data_output = 0
